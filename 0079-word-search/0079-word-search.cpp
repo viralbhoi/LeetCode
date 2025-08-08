7ea365c2c45c67df95b1b8c9c6a0a52186221955
Viral Bhoi <141637546+viralbhoi@users.noreply.github.com>
2025-08-08 23:37:35 +0530

    Time: 218 ms (74.25%), Space: 10.7 MB (60.64%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,53 +1,48 @@
 class Solution {
-    bool solve(int i,int j,int idx,vector<vector<char>> &mat, string& word,vector<vector<int>> &vis){
-        if(idx == word.size()){
+public:
+    bool dfs(int row, int col, vector<vector<char>>& grid,
+             vector<vector<int>>& vis, string& word, int current_index) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        if (current_index == word.size()) {
             return true;
         }
-        
-        int n = mat.size();
-        int m = mat[0].size();
-    
-        
-        int dir1[] = {0,1,0,-1};
-        int dir2[] = {1,0,-1,0};
-        
-        if(i<0 || i>=n || j<0 || j>= m || mat[i][j] != word[idx] || vis[i][j]==1){
+        if (!(row >= 0 && col >= 0 && row < n && col < m &&
+              vis[row][col] == 0 && grid[row][col] == word[current_index])) {
             return false;
         }
-        
-        vis[i][j] = 1;
-        
-        for(int d = 0;d<4;d++){
-            int ni = i + dir1[d];
-            int nj = j + dir2[d];
-            
-            if(solve(ni,nj,idx+1,mat,word,vis)){
+
+
+        vis[row][col] = 1;
+
+        int delta[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for (int i = 0; i < 4; i++) {
+            int nr = row + delta[i][0];
+            int nc = col + delta[i][1];
+
+            if (dfs(nr, nc, grid, vis, word, current_index + 1))
                 return true;
-            }
         }
-        vis[i][j] = 0;
-        
+
+        vis[row][col] = 0; // ✅ backtrack
         return false;
     }
-public:
+
     bool exist(vector<vector<char>>& board, string word) {
-        
         int n = board.size();
         int m = board[0].size();
-        
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j] == 0 && board[i][j] == word[0]){
-                    if(solve(i,j,0,board,word,vis)){
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] == 0 && board[i][j] == word[0]) {
+                    if (dfs(i, j, board, vis, word, 0))
                         return true;
-                    }
                 }
             }
         }
-        
-        return false;
 
+        return false;
     }
 };
