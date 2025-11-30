bc7faeab49b9f3151346e4ca853b11f82988e4f2
Viral Bhoi <141637546+viralbhoi@users.noreply.github.com>
2025-11-30 08:32:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        long long sum = accumulate(nums.begin(), nums.end(), 0LL);
+
+        if (sum % p == 0) {
+            return 0;
+        }
+        int n = nums.size();
+        int need_to_remove = sum % p;
+        int min_len = INT_MAX;
+        map<int,int> mp;
+        long long prefix = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefix += nums[i];
+
+            int target = (prefix - need_to_remove + p) % p;
+
+            if (mp.find(target) != mp.end()) {
+                min_len = min(min_len, i - mp[target]);
+            }
+
+            mp[prefix % p] = i;
+        }
+
+        return min_len == INT_MAX ? -1 : min_len;
+    }
+};
